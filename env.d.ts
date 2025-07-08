0fd5a7bac5e13842e8c64930701f8f8931150c39
Ajin <ajinkp9745@gmail.com>
2025-07-08 22:54:38 +0530

    Add initial project structure and configuration for MeeraEstuff

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    SUPABASE_SERVICE_ROLE_KEY: string;
+    RAZORPAY_KEY_ID: string;
+    RAZORPAY_KEY_SECRET: string;
+  }
+}
