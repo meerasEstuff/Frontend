096fdc6f349bf339ff03588a7e290d0a826bcef9
Ajin <ajinkp9745@gmail.com>
2025-07-19 10:45:45 +0530

    added new id generation function

--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -1,10 +1,3 @@
 export function generateCustomerId(): string {
-  const prefix = "CUST-";
-  // Generate a random string using base-36 (0-9 and a-z),
-  // then take a substring to get 6 characters and convert to uppercase.
-  const randomAlphanumeric = Math.random()
-    .toString(36)
-    .substring(2, 8)
-    .toUpperCase();
-  return `${prefix}${randomAlphanumeric}`;
+  return Math.floor(100000 + Math.random() * 900000).toString();
 }
