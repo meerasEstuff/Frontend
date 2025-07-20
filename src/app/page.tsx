20bd76c02f511b0a6383fb5628fa004d3970493a
Ajin <ajinkp9745@gmail.com>
2025-07-20 17:34:19 +0530

    removed three dote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -450,7 +450,7 @@ export default function ProfessionalLandingPage() {
           </motion.div>
         </div>
       </section>
-      ---
+
       <section id="features" className="py-20 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
